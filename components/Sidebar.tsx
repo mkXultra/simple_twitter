2820f146e2f8f99155e143239dc415fbeaed3568
mkXultra <electrincgoat@gmail.com>
2024-09-09 15:37:12 +0900

    modify: Add icons to Sidebar component
    - Install react-icons package
    - Import FaHome, FaUser, and FaCog icons from react-icons/fa
    - Add icons to sidebar menu items
    - Update CSS to align icons with text

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import styles from '../styles/Sidebar.module.css'
+import { FaHome, FaUser, FaCog } from 'react-icons/fa'
 
 const Sidebar = () => {
   return (
     <div className={styles.sidebar}>
       <h2>サイドバー</h2>
       <ul>
-        <li>ホーム</li>
-        <li>プロフィール</li>
-        <li>設定</li>
+        <li><FaHome /> ホーム</li>
+        <li><FaUser /> プロフィール</li>
+        <li><FaCog /> 設定</li>
       </ul>
     </div>
   )
